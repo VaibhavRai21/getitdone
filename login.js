9918bc3bfd3f8dd3f2624f6bf2d1db7c740c8403
Vaibhav Rai <vaibhavrai2104@gmail.com>
2025-03-19 12:00:30 +0530

    Docker

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ function login() {
 
     if (username === "admin" && password === "1234") {  // Simple validation
         localStorage.setItem("loggedIn", "true");
-        window.location.href = "index.html";  // Redirect to Task Remainder
+        window.location.href = "main.html";  // Redirect to Task Remainder
     } else {
         alert("Invalid Username or Password");
     }
@@ -12,10 +12,10 @@ function login() {
 
 // Check login status and prevent unauthorized access
 document.addEventListener("DOMContentLoaded", function () {
-    if (window.location.pathname.includes("index.html")) {
+    if (window.location.pathname.includes("main.html")) {
         const isLoggedIn = localStorage.getItem("loggedIn");
         if (!isLoggedIn) {
-            window.location.href = "login.html";  // Redirect if not logged in
+            window.location.href = "index.html";  // Redirect if not logged in
         }
     }
 });
